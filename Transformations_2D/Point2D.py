b3b9636417d47d5187a2a70d6f28bc08ca24ab65
MhdHO98 <167417592+MhdHO98@users.noreply.github.com>
2024-05-14 11:30:43 +0400

    Update Point2D.py

--- Transformations_2D/Point2D.py
+++ Transformations_2D/Point2D.py
@@ -1,1 +1,16 @@
+class Point2D:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    
+    def __add__(self, other):
+        return Point2D(self.x + other.x, self.y + other.y)
 
+    def __repr__(self):
+        return f"Point2D({self.x}, {self.y})"
+
+    def plot(self, color='r'):
+        plt.plot(self.x, self.y, marker='o', color=color)
+    
+    def __lt__(self, other):
+        return self.x < other.x
